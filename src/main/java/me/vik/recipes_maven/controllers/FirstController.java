e8d0a9f02fcae1af19914d65fe11735df8453909
VitaliyK <Vik163tlt@ya.ru>
2022-12-27 21:05:37 +0300

    Maven

---
+++ src/main/java/me/vik/recipes_maven/controllers/FirstController.java
@@ -0,0 +1,19 @@
+package me.vik.recipes_maven.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping("/")
+    public String startProject() {
+        return "Сайт рецептов - запущен!";
+    }
+
+    @GetMapping("/info")
+    public String getProjectInfo() {
+        return "Имя ученика: Конченков Виталий<br>Название проекта: Сайт рецептов<br> Дата создания: 10.12.2022<br>Описание проекта: " +
+                "Сайт рецептов, написан на Java с использованием Spring и Maven";
+    }
+}
